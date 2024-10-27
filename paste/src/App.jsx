56d7f0cccfca73d57a8706900cc3f0734a897de7
avinashdod <avinash.doodleblue@gmail.com>
2024-10-28 01:20:30 +0530

    paste application

---
+++ paste/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Home from './components/Home';
+import Paste from './components/Paste';
+import ViewPaste from './components/ViewPaste';
+import Navbar from './components/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+    <div className='w-full h-full flex flex-col'>
+      <Navbar/>
+      <Home/>
+    </div>
+  },
+  {
+    path: "/pastes",
+    element:
+    <div className='w-full h-full flex flex-col'>
+      <Navbar/>
+      <Paste/>
+    </div>
+  },
+  {
+    path: "/pastes/:id",
+    element:
+    <div className='w-full h-full flex flex-col'>
+      <Navbar/>
+      <ViewPaste/>
+    </div>
+  }
+])
+
+
+
+const App = () => {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
